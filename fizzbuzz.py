b2f5996f388ec1245a7b72a6594556c152606e8f
hyejic <hyeji0314@gmail.com>
2021-11-23 17:30:55 +0900

    feat: Add 3 list
    To test trailing comma

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,16 @@
+fruits = ['Apple', 'Banana', 'Pineapple']
+
+animals = [
+  'Lion',
+  'Tiger',
+  'Rabbit'
+]
+
+Clubs = [
+  'ManU',
+  'manC',
+]
+
 for i in range(1,16+1):
   if i%15==0:
      print('fizzbuzz')
